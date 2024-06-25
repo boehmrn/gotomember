141d62e38fb204cbfba351dd1cae17136229887a
Robin Böhm <robinboehm@hotmail.com>
2024-06-25 09:08:44 +0200

    asdf

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,29 @@
 "use strict";
+import { dir } from "console";
+import { isDataView } from "util/types";
 import * as vscode from "vscode";
-
+type Root = { children: vscode.DocumentSymbol[] };
+function format(str: string | number, length = 20) {
+  str = str.toString();
+  if (str.length > length) {
+    // String kürzen, wenn er zu lang ist
+    return str.substring(0, length);
+  } else {
+    // String mit Leerzeichen auffüllen, wenn er zu kurz ist
+    return str.padEnd(length);
+  }
+}
 export function activate(context: vscode.ExtensionContext) {
+  const log = (string?: string | number) => {
+    const naw = new Date().getTime();
+    const diff = naw - now;
+    console.log(format(diff.toString()), format(string || ""), int++);
+  };
   let symbols: vscode.DocumentSymbol[] = [];
+  let int = 0;
+  let now = new Date().getTime();
   let dirtyTree = true;
-  const root = { children: symbols };
+  const root: Root = { children: [] };
 
   const refreshSymbols = async (editor: vscode.TextEditor) => {
     if (dirtyTree || symbols.length === 0) {
@@ -12,7 +31,11 @@ export function activate(context: vscode.ExtensionContext) {
         "vscode.executeDocumentSymbolProvider",
         editor.document.uri
       );
-      root.children = symbols;
+      if (symbols) {
+        symbols = symbols.sort(sort);
+        root.children = symbols;
+      }
+      dirtyTree = false;
     }
   };
 
@@ -29,61 +52,132 @@ export function activate(context: vscode.ExtensionContext) {
       symbols = [];
     }
   );
+  function isBetweenSymbols(
+    position: vscode.Position,
+    symbols: vscode.DocumentSymbol[],
+    index: number,
+    before?: boolean
+  ) {
+    if (before) {
+      const symbol = symbols[index];
+      if (index === 0) {
+        return symbol.range.contains(position);
+      } else {
+        const symbol2 = symbols[index - 1];
+        return (
+          symbol.range.end.isAfter(position) &&
+          symbol2.range.end.isBeforeOrEqual(position)
+        );
+      }
+    } else {
+      const symbol = symbols[index];
+      if (index === symbols.length - 1) {
+        return symbol.range.contains(position);
+      }
+      const symbol2 = symbols[index + 1];
+      return (
+        symbol.range.start.isBeforeOrEqual(position) &&
+        symbol2.range.start.isAfter(position)
+      );
+    }
+  }
+  function findSymbolIndex(
+    position: vscode.Position,
+    symbols: vscode.DocumentSymbol[],
+    before?: boolean
+  ) {
+    if (before) {
+      return symbols.findIndex((symbol) => symbol.range.end.isAfter(position));
+    } else {
+      const index = [...symbols]
+        .reverse()
+        .findIndex((symbol) => symbol.range.start.isBeforeOrEqual(position));
+
+      return index === -1 ? index : symbols.length - index - 1;
+    }
+  }
+  function isInRange(
+    position: vscode.Position,
+    symbols: vscode.DocumentSymbol[]
+  ) {
+    return (
+      symbols.at(0)?.range.start.isBeforeOrEqual(position) &&
+      symbols.at(-1)?.range.end.isAfter(position)
+    );
+  }
 
   function findSymbolAtPositionWithParent(
-    symbols: vscode.DocumentSymbol[],
     position: vscode.Position,
-    parentSymbol:
-      | vscode.DocumentSymbol
-      | undefined
-      | { children: vscode.DocumentSymbol[] }
+    parentSymbol: vscode.DocumentSymbol | Root = root,
+    before?: boolean
   ): {
-    parentSymbol:
-      | vscode.DocumentSymbol
-      | undefined
-      | { children: vscode.DocumentSymbol[] };
-    childSymbol: vscode.DocumentSymbol | undefined;
+    parentSymbol: vscode.DocumentSymbol | Root;
+    childSymbol?: vscode.DocumentSymbol;
+    included: boolean;
+    index: number;
   } {
-    for (const symbol of symbols) {
-      if (symbol.range.contains(position)) {
-        if (symbol.children.length > 0) {
-          const { parentSymbol: newParentSymbol, childSymbol } =
-            findSymbolAtPositionWithParent(symbol.children, position, symbol);
-          if (childSymbol) {
-            return { parentSymbol: newParentSymbol, childSymbol };
-          } else {
-            return { parentSymbol, childSymbol: symbol };
-          }
-        }
-        return { parentSymbol, childSymbol: symbol };
-      }
+    if (!parentSymbol.children?.length) {
+      return {
+        parentSymbol,
+        included: false,
+        index: -1,
+      };
     }
-    return { parentSymbol, childSymbol: undefined };
+    parentSymbol.children = parentSymbol.children.sort(sort);
+    const index = findSymbolIndex(position, parentSymbol.children, before);
+    if (
+      index === -1 ||
+      !parentSymbol.children[index].range.contains(position)
+    ) {
+      return {
+        parentSymbol,
+        included: false,
+        index,
+      };
+    } else if (
+      parentSymbol.children[index].range.start.isEqual(position) ||
+      !parentSymbol.children[index].children.length
+    ) {
+      return {
+        parentSymbol,
+        included: true,
+        index,
+      };
+    } else {
+      return findSymbolAtPositionWithParent(
+        position,
+        parentSymbol.children[index],
+        before
+      );
+    }
   }
   function findParentSymbol(
-    symbols: vscode.DocumentSymbol[],
-    target: vscode.DocumentSymbol
-  ): vscode.DocumentSymbol | undefined {
-    for (const symbol of symbols) {
-      if (symbol.children.includes(target)) {
-        return symbol;
-      }
-      const parent = findParentSymbol(symbol.children, target);
-      if (parent) {
+    parent: vscode.DocumentSymbol | Root,
+    symbol: vscode.DocumentSymbol
+  ): vscode.DocumentSymbol | Root | undefined {
+    for (const child of parent.children) {
+      if (child === symbol) {
         return parent;
+        break;
+      } else {
+        const newParent = findParentSymbol(child, symbol);
+        if (newParent) {
+          return newParent;
+        }
       }
     }
-    return undefined;
   }
   function getFirstChild(
     symbol: vscode.DocumentSymbol,
-    parentSymbol: vscode.DocumentSymbol | { children: vscode.DocumentSymbol[] }
-  ): vscode.DocumentSymbol {
+    parentSymbol: vscode.DocumentSymbol | Root,
+    first?: boolean
+  ): vscode.DocumentSymbol | undefined {
     if (symbol.children.length > 0) {
       const children = symbol.children.sort(sort);
       return children[0];
     } else {
-      return getSibling(symbol, parentSymbol, true);
+      if (first) {
+      }
     }
   }
   const sort = (a: vscode.DocumentSymbol, b: vscode.DocumentSymbol) => {
@@ -92,33 +186,6 @@ export function activate(context: vscode.ExtensionContext) {
     }
     return a.range.start.line - b.range.start.line;
   };
-  function getSibling(
-    symbol: vscode.DocumentSymbol,
-    parent: vscode.DocumentSymbol | { children: vscode.DocumentSymbol[] },
-    next: boolean
-  ): vscode.DocumentSymbol {
-    const children = parent.children.sort(sort);
-
-    const index = children.indexOf(symbol);
-
-    if (next) {
-      if (index + 1 < children.length) {
-        return children[index + 1];
-      } else if ("range" in parent) {
-        const grandParent = findParentSymbol(symbols, parent);
-        if (grandParent && "range" in grandParent) {
-          return getSibling(parent, grandParent, true);
-        }
-      }
-    } else {
-      if (index - 1 >= 0) {
-        return children[index - 1];
-      } else if ("range" in parent) {
-        return parent;
-      }
-    }
-    return symbol;
-  }
   function goToSymbol(
     editor: vscode.TextEditor,
     symbol: vscode.DocumentSymbol
@@ -128,98 +195,109 @@ export function activate(context: vscode.ExtensionContext) {
     editor.selection = newSelection;
     editor.revealRange(
       new vscode.Range(position, position),
-      vscode.TextEditorRevealType.InCenter
+      vscode.TextEditorRevealType.Default
     );
   }
 
   const nextMemberCommand = vscode.commands.registerTextEditorCommand(
     "gotoMember.next",
     async (editor: vscode.TextEditor) => {
+      now = new Date().getTime();
       await refreshSymbols(editor);
-      if (symbols.length === 0) {
-        return;
-      }
-      const { parentSymbol, childSymbol } = findSymbolAtPositionWithParent(
-        symbols,
-        editor.selection.active,
-        root
+      const position = editor.selection.active;
+      const { parentSymbol, included, index } = findSymbolAtPositionWithParent(
+        position,
+        root,
+        true
       );
-      if (!childSymbol || !parentSymbol) {
+      if (index === -1 || index === parentSymbol.children.length - 1) {
         return;
       }
-      const siblingSymbol = getSibling(childSymbol, parentSymbol, true);
-
-      goToSymbol(editor, siblingSymbol);
-      vscode.window.setStatusBarMessage("Next Member", 1000);
+      if (!included) {
+        goToSymbol(editor, parentSymbol.children[index]);
+      } else {
+        goToSymbol(editor, parentSymbol.children[index + 1]);
+      }
     }
   );
   const previousMemberCommand = vscode.commands.registerTextEditorCommand(
     "gotoMember.previous",
     async (editor: vscode.TextEditor) => {
+      now = new Date().getTime();
       await refreshSymbols(editor);
-      if (symbols.length === 0) {
-        return;
-      }
-      const { parentSymbol, childSymbol } = findSymbolAtPositionWithParent(
-        symbols,
-        editor.selection.active,
-        root
+      const position = editor.selection.active;
+      const { parentSymbol, included, index } = findSymbolAtPositionWithParent(
+        position,
+        root,
+        false
       );
-      if (!childSymbol || !parentSymbol) {
+      if (index === -1 || index === 0) {
         return;
       }
-      const siblingSymbol = getSibling(childSymbol, parentSymbol, false);
-
-      goToSymbol(editor, siblingSymbol);
-      vscode.window.setStatusBarMessage("Previous Member", 1000);
+      if (!included) {
+        goToSymbol(editor, parentSymbol.children[index]);
+      } else {
+        goToSymbol(editor, parentSymbol.children[index - 1]);
+      }
     }
   );
   const parentMemberCommand = vscode.commands.registerTextEditorCommand(
     "gotoMember.parent",
     async (editor: vscode.TextEditor) => {
+      now = new Date().getTime();
       await refreshSymbols(editor);
-      if (symbols.length === 0) {
+
+      const position = editor.selection.active;
+      const { parentSymbol, included, index } = findSymbolAtPositionWithParent(
+        position,
+        root,
+        false
+      );
+      if (index === -1) {
         return;
       }
-      const { parentSymbol, childSymbol } = findSymbolAtPositionWithParent(
-        symbols,
-        editor.selection.active,
-        root
-      );
-      if (!parentSymbol) {
+      if (!("range" in parentSymbol)) {
         return;
-      } else if (!("range" in parentSymbol) && childSymbol) {
-        const newParentSymbol = getSibling(childSymbol, parentSymbol, false);
-        if (newParentSymbol) {
-          goToSymbol(editor, newParentSymbol);
-        }
-      } else if ("range" in parentSymbol) {
-        goToSymbol(editor, parentSymbol);
       }
 
-      vscode.window.setStatusBarMessage("Parent Member", 1000);
+      goToSymbol(editor, parentSymbol);
     }
   );
 
   const childMemberCommand = vscode.commands.registerTextEditorCommand(
     "gotoMember.child",
     async (editor: vscode.TextEditor) => {
+      now = new Date().getTime();
       await refreshSymbols(editor);
       if (symbols.length === 0) {
         return;
       }
-      const { parentSymbol, childSymbol } = findSymbolAtPositionWithParent(
-        symbols,
-        editor.selection.active,
-        root
+      const position = editor.selection.active;
+      let { parentSymbol, included, index } = findSymbolAtPositionWithParent(
+        position,
+        root,
+        true
       );
-      if (!childSymbol || !parentSymbol) {
+      if (index === -1) {
         return;
       }
-      const firstChild = getFirstChild(childSymbol, parentSymbol);
+      if (!included) {
+        goToSymbol(editor, parentSymbol.children[index]);
+        return;
+      }
+      const newIndex = parentSymbol.children
+        .slice(index)
+        .findIndex((symbol) => {
+          return symbol.children.length > 0;
+        });
+      if (newIndex === -1) {
+        return;
+      }
 
-      goToSymbol(editor, firstChild);
-      vscode.window.setStatusBarMessage("Child Member", 1000);
+      goToSymbol(
+        editor,
+        parentSymbol.children[index + newIndex].children.sort(sort)[0]
+      );
     }
   );
 
